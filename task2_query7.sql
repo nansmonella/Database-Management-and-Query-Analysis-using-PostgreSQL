0ed567efe7a91c9127b5a467d38fee9240d5d881
nansmonella <65117881+nansmonella@users.noreply.github.com>
2023-06-28 02:13:22 +0300

    Update task2_query7.sql

--- task2_query7.sql
+++ task2_query7.sql
@@ -1,3 +1,6 @@
+--Show top 10 rated electronics (eletronicos) products that has been rated at least 5 times. 
+--For each product in top 10 list, show product id, review avg, review count; with descending order for review avg, descending order for review count
+
 select 
 	oi.product_id, 
 	COUNT(or2.review_score) AS review_count,
